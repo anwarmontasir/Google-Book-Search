8e7fb91e117a69e6f918d4a0bf7cc9fa6baade24
Anwar Montasir <anwarmontasir@gmail.com>
2019-06-10 21:44:36 -0700

    some style

--- src/SearchResult/SearchResult.js
+++ src/SearchResult/SearchResult.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './SearchResult.css';
 
+const Entities = require('html-entities').XmlEntities;
+ 
+const entities = new Entities();
+
 export default function SearchResults(props) {
     const authors = props.volumeInfo.authors.join(', ');
     const price = props.saleInfo.listPrice ? <p>Price: ${props.saleInfo.listPrice.amount}</p> : '';
+    const textSnippet = props.searchInfo ? entities.decode(props.searchInfo.textSnippet) : entities.decode(props.volumeInfo.description);
     
     return (
         <div className="searchResult">
@@ -14,7 +19,7 @@ export default function SearchResults(props) {
                 <h2>{props.volumeInfo.title}</h2>
                 <p>Author: {authors}</p>
                 {price}
-                <p>{props.searchInfo.textSnippet}</p>
+                <p className="textSnippet">{textSnippet}</p>
             </div>
         </div>
     )
